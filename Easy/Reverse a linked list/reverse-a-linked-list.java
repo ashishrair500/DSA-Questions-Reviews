3a3bb7071157f9258e2817d6dd13e1dba96883ad
Ashish Rai <100395176+ashishrair500@users.noreply.github.com>
2023-08-03 22:26:38 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reverse a linked list/reverse-a-linked-list.java
@@ -0,0 +1,94 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG{
+	static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+    public static void main(String args[]) throws IOException { 
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t > 0){
+        
+        	int n = sc.nextInt();
+        
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+        
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+        
+            Solution ob = new Solution();
+            head = ob.reverseList(head);
+            printList(head); 
+            t--;
+        }
+    } 
+} 
+   
+// } Driver Code Ends
+
+
+//function Template for Java
+
+/* linked list node class:
+
+class Node {
+    int data;
+    Node next;
+    Node(int value) {
+        this.value = value;
+    }
+}
+
+*/
+
+class Solution
+{
+    //Function to reverse a linked list.
+    Node reverseList(Node head)
+    {
+        ArrayList<Integer> as= new ArrayList<>();
+        
+        Node temp=head;
+        while(temp!=null){
+            as.add(temp.data);
+            temp=temp.next;
+        }
+        head=temp;
+    Node node= new Node(as.get(0));
+head=node;
+        
+        for(int i=1; i<as.size(); i++){
+            node= new Node(as.get(i));
+            node.next=head;
+            head=node;
+            
+            
+        }
+        return head;
+        // code here
+    }
+}
